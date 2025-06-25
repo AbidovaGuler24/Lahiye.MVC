b5545ec3f4422ef6707092490dc83ce0de7ede19
Guler <gullaraa-ab109@code.edu.az>
2025-06-25 18:46:39 +0400

    commit25.2

--- Lahiye.Mvc/obj/Debug/net8.0/Lahiye.Mvc.AssemblyInfo.cs
+++ Lahiye.Mvc/obj/Debug/net8.0/Lahiye.Mvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lahiye.Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91769319c00c147a2ec2f4ef69ded26db46bca74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e418c075f46e0022d2736da10524a27119340345")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lahiye.Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lahiye.Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OnlineLearning.BL/obj/Debug/net8.0/OnlineLearning.BL.AssemblyInfo.cs
+++ OnlineLearning.BL/obj/Debug/net8.0/OnlineLearning.BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineLearning.BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91769319c00c147a2ec2f4ef69ded26db46bca74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e418c075f46e0022d2736da10524a27119340345")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineLearning.BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineLearning.BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
